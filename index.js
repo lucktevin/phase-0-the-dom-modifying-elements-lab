48dcad81e4e5513c9a4f9f9072dcf95735f72507
Luck Tevin <luck.tevin@student.moringaschool.com>
2023-09-30 17:49:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-// Write your code here!
+// Write your code here!function addingEventListener() {
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
+}
+
+addingEventListener(); // Call the outer function to activate the event listener
+
 const newHeader =document.getElementById("victory")
